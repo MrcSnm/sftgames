c0aa119dc0c86ac23cb31a5093fd7ad181e53cfb
MrcSnm <msnmancini@hotmail.com>
2024-02-28 07:30:14 -0300

    Added: FUllscreen + orientation lock on landscape

--- src/scenes/NavigatedScene.ts
+++ src/scenes/NavigatedScene.ts
@@ -8,6 +8,16 @@ export class NavigatedScene extends Scene
 {
     create ()
     {
+        this.input.on('pointerdown', () => 
+        {
+            if(!this.scale.isFullscreen)
+                this.scale.startFullscreen()
+            if(screen.orientation.type != 'landscape-primary')
+            {
+                if(!this.sys.game.device.os.desktop)
+                    (screen.orientation as any).lock('landscape-primary');
+            }
+        });
         this.add.existing(new Navigator(this));
     }
 }
